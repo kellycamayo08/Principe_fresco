a325867bf7e384f1f7575a3de60f7b34a539b7d7
Geiler Prada <geilerprada7@gmail.com>
2022-05-20 16:54:24 -0500

    cart closed

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,32 @@
-import { getData } from '../helpers/getData.js'
-import { verProductos } from '../modules/verProductos.js';
+import { getData } from "../helpers/getData.js";
+import { verProductos } from "../modules/verProductos.js";
 
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const data = await getData('products');
-    verProductos(data, container);
-})
+document.addEventListener("DOMContentLoaded", async () => {
+  const data = await getData("products");
+  verProductos(data, container);
+});
 
-document.addEventListener('click', async ({ target }) => {
-    const data = await getData('products');
+document.addEventListener("click", async ({ target }) => {
+  const data = await getData("products");
 
-    if (target.classList.contains('product')) {
-        const producto = data.find(item => item.id == target.id);
-        console.log(producto);
-        sessionStorage.setItem('detalle', JSON.stringify(producto));
-        window.location.href = './pages/detalle.html';
-    }
-})
+  if (target.classList.contains("product")) {
+    const producto = data.find(item => item.id == target.id);
+    console.log(producto);
+    sessionStorage.setItem("detalle", JSON.stringify(producto));
+    window.location.href = "./pages/detalle.html";
+  }
+});
+
+//open card
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+// Close cart
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
