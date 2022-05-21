03f4ebbc295b410942a19c07558f997f4d2a01b5
Kelly Camayo <Kelly Camayo>
2022-05-21 11:46:46 -0500

    cambiando lineacion de img

--- modules/verProductos.js
+++ modules/verProductos.js
@@ -3,7 +3,7 @@ export const verProductos = (data, cont) => {
 
   data.forEach(({ image, title, price, id }) => {
     cont.innerHTML += `
-        <div class="card m-5 product" style="width: 18rem;">
+        <div class="card m-3 product" style="width: 18rem;">
             <img id="${id}" class="product" src="${image}" alt="...">
             <div id="${id}" class="card-body product">
                 <h5 id="${id}" class="card-title product">${title}</h5>
