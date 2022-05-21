f1149117dd57dc4d58cf48f7a4ed5e95bc9d9626
Kelly Camayo <Kelly Camayo>
2022-05-21 12:33:43 -0500

    final

--- modules/verProductos.js
+++ modules/verProductos.js
@@ -4,10 +4,10 @@ export const verProductos = (data, cont) => {
   data.forEach(({ image, title, price, id }) => {
     cont.innerHTML += `
         <div class="card m-3 product" style="width: 18rem;">
-            <img id="${id}" class="product" src="${image}" alt="...">
+            <img id="${id}" class="product"  src="${image}" width="286" height="450" alt="...">
             <div id="${id}" class="card-body product">
-                <h5 id="${id}" class="card-title product">${title}</h5>
-                <p class="card-text mt-4"> Precio: <strong>${price}</strong></p>
+                <h5 id="${id}" class="card-title product text-center">${title}</h5>
+                <p class="card-text mt-4 text-center fs-5"> Precio: <strong>$ ${price}</strong></p>
             </div>
         </div>
         `;
